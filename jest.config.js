6e9eaafc8bb31343bd9aed005580686b97007f31
Paul Guerra <paulrguerra@gmail.com>
2019-03-03 00:33:39 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,ts}', '!**/__tests__/**/*.*', '!**/__mocks__/**/*.*'],
   testPathIgnorePatterns: ['node_modules', 'tmp/', 'dist/', '.*\.perf\.ts'],
 };
